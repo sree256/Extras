745024c11922f32bf00f5e4f0c29bfb7a8edf890
Sreejith <sreejith.sa@zohocorp.com>
2023-07-03 12:08:45 +0530

    files added

---
+++ Sources/extrasTarget/dummy.m
@@ -0,0 +1,8 @@
+//
+//  dummy.m
+//  
+//
+//  Created by sreejith-zt604 on 28/06/23.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Sources/extrasTarget/include/dummy.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  
+//
+//  Created by sreejith-zt604 on 28/06/23.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
